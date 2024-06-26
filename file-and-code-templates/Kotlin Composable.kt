ec94cbb3fe905d597fb7741e8399bd1f8af3fe87
Hevin Mun <34952833+hevinxx@users.noreply.github.com>
2024-06-26 23:02:18 +0900

    Fix the 'if' clause in 'Kotlin Composable'.

--- file-and-code-templates/Kotlin Composable.kt
+++ file-and-code-templates/Kotlin Composable.kt
@@ -1,8 +1,9 @@
 #if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME}
 
+#end
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-#end
+
 #parse("File Header.java")
 @Composable
 fun ${NAME}() {
